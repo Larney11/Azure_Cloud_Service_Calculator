352d7ddc2ea234805cd55345a0675a8a673bd0b8
Larney11 <larharneycomputing@gmail.com>
2017-09-14 18:20:58 +0100

    Add project files.

---
+++ Azure_Cloud_Service_Calculator/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Azure_Cloud_Service_Calculator
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Azure_Cloud_Service_Calculator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Azure_Cloud_Service_Calculator.Startup))]
+namespace Azure_Cloud_Service_Calculator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
